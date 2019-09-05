23f98a93b4b0f39392f983a05f79b3fe426c3716
Peja016 <jerry79525@gmail.com>
2019-09-05 18:42:10 +0800

    updated summaryPage

--- src/containers/SummaryPage/index.js
+++ src/containers/SummaryPage/index.js
@@ -28,11 +28,11 @@ class SummaryPage extends PureComponent {
           <Box px="2em" width={1/3} key={index}>
             <Box bg="lightGray" my="2em" borderRadius="5em" py="1.5em">
               <Flex alignItems="center">
-                <Box width={3/10} textAlign="right" borderRight="1px solid" pr="1em" py="0.5em">
+                <Box width={3/10} textAlign="right" borderRight="2px solid" pr="1em" py="0.5em">
                   <Text.inline fontSize="2em" color={color}>{value}</Text.inline>
                   <Text.inline pl="0.25em">{unit}</Text.inline>
                 </Box>
-                <Box width={7/10} whiteSpace="pre" borderLeft="1px solid" pl="1em" py="0.5em">
+                <Box width={7/10} whiteSpace="pre" pl="1em" py="0.5em">
                   {label.replace(String.fromCharCode(92).concat('n'), '\n')}
                 </Box>
               </Flex>
@@ -46,13 +46,13 @@ class SummaryPage extends PureComponent {
         <PatternBg py="2em">
           <Container>
             <Box fontSize="1.5em" borderBottom="1px solid" py="0.75em">
-              <Text letterSpacing={2} fontWeight="bold">案件情況</Text>
+              <Text letterSpacing={2} fontWeight="bold" color="textBlue">案件情況</Text>
             </Box>
-            <Module datas={primarys} color="orange2" my="2em" />
+            <Module datas={primarys} color="textYellow" my="2em" />
             <LineBg>
               <Box height="2em" />
             </LineBg>
-            <Module datas={secondarys} color="darkBlue" />
+            <Module datas={secondarys} color="textBlue" />
           </Container>
         </PatternBg>
       </Layout>
