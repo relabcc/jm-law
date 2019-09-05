128a97553863dfe980fbed64a2edb2ec72b31013
Renddi Yeh <renddi@relab.cc>
2019-09-05 14:58:08 +0800

    updated

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import ReactDOM from 'react-dom';
 import WithProvider from './with-provider'
 import App from './App';
 
-ReactDOM.render(<WithProvider><App /></WithProvider>, document.getElementById('root'));
-
+if (document.getElementById('root')) {
+  ReactDOM.render(<WithProvider><App /></WithProvider>, document.getElementById('root'));
+}
