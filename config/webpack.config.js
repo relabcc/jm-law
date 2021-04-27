3852e2b2cfccda461bb6e4c2ab6e5549c0e36bfe
Renddi Yeh <renddi@relab.cc>
2021-04-27 12:04:44 +0800

    fixed webpack

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -521,27 +521,6 @@ module.exports = function(webpackEnv) {
             : undefined
         )
       ),
-      new HtmlWebpackPlugin(
-        Object.assign(
-          {},
-          isEnvProduction
-            ? {
-                minify: {
-                  removeComments: true,
-                  collapseWhitespace: true,
-                  removeRedundantAttributes: true,
-                  useShortDoctype: true,
-                  removeEmptyAttributes: true,
-                  removeStyleLinkTypeAttributes: true,
-                  keepClosingSlash: true,
-                  minifyJS: true,
-                  minifyCSS: true,
-                  minifyURLs: true,
-                },
-              }
-            : undefined
-        )
-      ),
       // Inlines the webpack runtime script. This script is too small to warrant
       // a network request.
       isEnvProduction &&
